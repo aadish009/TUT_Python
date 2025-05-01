5b2834b714148a6c6e330dd8c924eb906802b208
aadish788 <adish.bhat@cardinaltsinc.net>
2025-05-01 15:30:19 +0530

    Assignment1

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+# Task 1 
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+print("Addidtion: ", int(num1) + int(num2))
+print("Subtraction: ", int(num1) - int(num2))
+print("Multiplication: ", int(num1) * int(num2))
+print("Division: ", int(num1) / int(num2))
+
+
+# Task 2 
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello," + first_name +" " + last_name + "!" + " Welcome to Python programming.")
+
+
